2b0372e2783e29c06a598d28f62458f9167f903c
KaarTechAS <94947062+KaarTechAS@users.noreply.github.com>
2021-11-30 12:32:33 +0530

    full logic in service file

--- Anish/service/service.js
+++ Anish/service/service.js
@@ -6,29 +6,33 @@ const mysql = require('mysql');
 //Refernece:
 //https://www.qat.com/simple-rest-service-node-js-express/
 
-Service = function() {
-    this.getRecords = function(startv, endv, cb){
-        var query = "SELECT * FROM m_student LIMIT ?,?;";
-        query = mysql.format(query,[
-            startv,
-            endv
-        ]);
-        /*
-            {
-                "index":,
-                "nor":
-            }
-        */
-        connection.query(query, (err, result)=>{
-            if(err){
-                console.log(err);
-                cb(null, Promise.reject("Couldn't find data!"), "null");
-            }
-            else{
-                cb(null, Promise.resolve(result), result);
-            }
-        });
-    };
+getRecMysql = (startv, endv, cb) => {
+    var query = "SELECT * FROM m_student LIMIT ?,?;";
+    query = mysql.format(query,[
+        startv,
+        endv
+    ]);
+    /*
+        {
+            "index":,
+            "nor":
+        }
+    */
+    connection.query(query, (err, result)=>{
+        if(err){
+            console.log(err);
+            cb(null, Promise.reject("Couldn't find data!"), "null");
+        }
+        else{
+            cb(null, Promise.resolve(result), result);
+        }
+    });
 };
 
-exports.Service = Service;
+module.exports.getRecords = async (req,res)=>{
+    let startv = parseInt(req.body.index);
+    let endv = parseInt(req.body.nor);
+    await getRecMysql(startv, endv, function(error, result, resstr) {
+        result.then(res.send(resstr));
+      });
+}
